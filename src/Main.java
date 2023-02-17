9f0b825fce52e98afdf38308e58a4d08ac45346f
Crashingleo <Leo2872497@gmail.com>
2023-02-17 13:52:23 +0400

    Hello world!!

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         task2();
         task3();
         task4();
+        task5();
 
     }
 
@@ -23,28 +24,28 @@ public class Main {
         System.out.println("Second Task");
         int clientOS = 1;
         int clientDeviceYear = 2017;
-            switch (clientOS)
-            {
-                case 0:
-                    if (clientDeviceYear < 2015) {
-                        System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-                    } else {
-                        System.out.println("Установите версию приложения для iOS по ссылке");
-                    }
-                    break;
-                case 1:
-                    if (clientDeviceYear < 2015) {
-                        System.out.println("Установите облегченную версию приложения для Android по ссылке");
-                    } else {
-                        System.out.println("Установите версию приложения для Android по ссылке");
-                    }
-                    break;
-                default:
-                    System.out.println("eror");
-                    break;
-            }
+        switch (clientOS) {
+            case 0:
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                }
+                break;
+            case 1:
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                }
+                break;
+            default:
+                System.out.println("eror");
+                break;
+        }
 
     }
+
     public static void task3() {
         System.out.println("Task 3");
         int year = 1;
@@ -62,15 +63,70 @@ public class Main {
             System.out.println("Год не високосный");
         }
     }
-    public static void task4(){
+
+    public static void task4() {
         System.out.println("Task 4");
+        int deliveryDistance = 95;
+        if (deliveryDistance <= 20) {
+            System.out.println("Потребуется дней: 1");
+        } else if (deliveryDistance < 60 && deliveryDistance > 20) {
+            System.out.println("Потребуется дней: 2");
+
+        } else if (deliveryDistance <= 100 && deliveryDistance >= 60) {
+            System.out.println("Потребуется дней: 3");
+        } else {
+            System.out.println("Доставки нет");
+        }
+
 
     }
+    public static void task5(){
+        System.out.println("Task5");
+        int month = 1;
+        switch (month){
+            case 1:
+                System.out.println("Январь");
+                break;
+            case 2:
+                System.out.println("Февраль");
+                break;
+            case 3:
+                System.out.println("Март");
+                break;
+            case 4:
+                System.out.println("Апрель");
+                break;
+            case 5:
+                System.out.println("Май");
+                break;
+            case 6:
+                System.out.println("Июнь");
+                break;
+            case 7:
+                System.out.println("Июль");
+                break;
+            case 8:
+                System.out.println("Авгусо");
+                break;
+            case 9:
+                System.out.println("Сентябрь");
+                break;
+            case 10:
+                System.out.println("Октябрь");
+                break;
+            case 11:
+                System.out.println("Ноябрь");
+                break;
+            case 12:
+                System.out.println("Декабрь");
+                break;
+            default:
+                System.out.println("Неправильный номер месяца");
 
-
-
-
+        }
     }
+}
+
 
 
 
